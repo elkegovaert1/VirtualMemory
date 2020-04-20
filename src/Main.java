e6e05cd4e5e933a4ab6b6730373a3cf1e3f48455
elkegovaert <elke.govaert@outlook.com>
2020-04-20 18:06:05 +0200

    Schoonheidsfoutjes

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
         primaryStage.setTitle("Virtual Memory");
-        primaryStage.setScene(new Scene(root, 1000, 750));
+        primaryStage.setScene(new Scene(root, 1200, 600));
         primaryStage.show();
     }
 
