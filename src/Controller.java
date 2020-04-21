ffeb6317eea58582821ca7e6d536436ff2a3cf34
glenn <glenn.groothuis@gmail.com>
2020-04-21 09:02:05 +0200

    Activate buttons

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,7 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextInputDialog;
 
 public class Controller {
 
@@ -11,4 +13,26 @@ public class Controller {
 
     @FXML
     private Button restart;
+
+    @FXML //Execute one instruction and update the view
+    void executeOneInstruction(ActionEvent event) {
+
+        TextInputDialog dialog = new TextInputDialog();
+        //dialog.initOwner(stage); //nodig om bij een gemaximaliseerde toepassing goed te verschijnen
+        dialog.setTitle("Toevoegen");
+        dialog.setHeaderText("AfdelingJurylid");
+        dialog.setContentText("ID");
+        dialog.setGraphic(null);
+        int id = 0;
+        try {
+            id = Integer.parseInt(dialog.showAndWait().get());
+        } catch (Exception e) {
+        }
+    }
+
+    @FXML //Execute one instruction and update the view
+    void executeAll(ActionEvent event) {}
+
+    @FXML //Execute one instruction and update the view
+    void restart(ActionEvent event) {}
 }
